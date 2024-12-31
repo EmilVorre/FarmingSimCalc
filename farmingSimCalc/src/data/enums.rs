f8011c6f81e5637820d9ae88247005e584215fd5
VorHerre <evorre23@student.aau.dk>
2024-12-31 04:57:53 +0100

    Add initial project structure and define enums for crops, animals, and maps

---
+++ farmingSimCalc/src/data/enums.rs
@@ -0,0 +1,44 @@
+enum Crops {
+  Wheat,
+  Barley,
+  Canola,
+  Oats,
+  Corn,
+  Sunflower,
+  Soybeans,
+  Potatoes,
+  Rice,
+  DryRice,
+  SugarBeet,
+  SugarCane,
+  Cotton,
+  Sorghum,
+  Graps,
+  Olives,
+  Poplar,
+  RedBeet,
+  Carrot,
+  Parsnip,
+  GreenBean,
+  Peas,
+  Spinach,
+  Grass,
+}
+
+enum Animals {
+  Bees,
+  Chickens,
+  Cows,
+  Goats,
+  Horses,
+  Pigs,
+  Sheeps,
+  WaterBuffalo,
+}
+
+enum Maps {
+  RiverbendSprings,
+  HutanPantai,
+  Zielonka,
+}
+
---
+++ farmingSimCalc/src/data/fields.rs
@@ -0,0 +1,182 @@
+const FIELD_DATA: [Field; 90] = [
+    Field {
+        name: "Field 1",
+        size: 0,
+    },
+    Field {
+        name: "Field 2",
+        size: 1.82,
+    },
+    Field {
+        name: "Field 3",
+        size: 1,
+    },
+    Field {
+        name: "Field 4",
+        size: 0.61,
+    },
+    Field {
+        name: "Field 5",
+        size: 0.77,
+    },
+    Field {
+        name: "Field 6",
+        size: 1.74,
+    },
+    Field {
+        name: "Field 7",
+        size: 2,
+    },
+    Field {
+        name: "Field 8",
+        size: 2.21,
+    },
+    Field {
+        name: "Field 9",
+        size: 0.76,
+    },
+    Field {
+        name: "Field 10",
+        size: 0.81,
+    },
+    Field {
+        name: "Field 11",
+        size: 1.92,
+    },
+    Field {
+        name: "Field 12",
+        size: 1.13,
+    },
+    Field {
+        name: "Field 13",
+        size: 1.01,
+    },
+    Field {
+        name: "Field 14",
+        size: 0.55,
+    },
+    Field {
+        name: "Field 15",
+        size: 1.01,
+    },
+    Field {
+        name: "Field 16",
+        size: 1.04,
+    },
+    Field {
+        name: "Field 17",
+        size: 0,
+    },
+    Field {
+        name: "Field 18",
+        size: 0,
+    },
+    Field {
+        name: "Field 19",
+        size: 2.49,
+    },
+    Field {
+        name: "Field 20",
+        size: 2.04,
+    }, 
+    Field {
+        name: "Field 21",
+        size: 1.21,
+    },
+    Field {
+        name: "Field 22",
+        size: 2.29,
+    },
+    Field {
+        name: "Field 23",
+        size: 1.99,
+    },
+    Field {
+        name: "Field 24",
+        size: 6.81,
+    },
+    Field {
+        name: "Field 25",
+        size: 2.87,
+    },
+    Field {
+        name: "Field 26",
+        size: 1.21,
+    },
+    Field {
+        name: "Field 27",
+        size: 0.48,
+    },
+    Field {
+        name: "Field 28",
+        size: 0.56,
+    },
+    Field {
+        name: "Field 29",
+        size: 0.55,
+    },
+    Field {
+        name: "Field 30",
+        size: 1.68,
+    },
+    Field {
+        name: "Field 31",
+        size: 2.54,
+    },
+    Field {
+        name: "Field 32",
+        size: 0.91,
+    },
+    Field {
+        name: "Field 33",
+        size: 0,
+    },
+    Field {
+        name: "Field 34",
+        size: 0.21,
+    },
+    Field {
+        name: "Field 35",
+        size: 0.49,
+    },
+    Field {
+        name: "Field 36",
+        size: 1.72,
+    },
+    Field {
+        name: "Field 37",
+        size: 2.43,
+    },
+    Field {
+        name: "Field 38",
+        size: 4.37,
+    },
+    Field {
+        name: "Field 39",
+        size: 0,
+    },
+    Field {
+        name: "Field 40",
+        size: 1.95,
+    },
+    Field {
+        name: "Field 41",
+        size: 0.96,
+    },
+    Field {
+        name: "Field 42",
+        size: 0,
+    },
+    Field {
+        name: "Field 43",
+        size: 0.76,
+    },
+    Field {
+        name: "Field 44",
+        size: 0.71,
+    },
+    Field {
+        name: "Field 45",
+        size: 1.78,
+    },
+];
